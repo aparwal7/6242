3846c3cc58b055434d79330b8b32f352a08d9a6e
wsultan1 <53533775+wsultan1@users.noreply.github.com>
2021-04-17 07:21:18 +0300

    Update wards.py

--- workspace/ws_workspace/wards.py
+++ workspace/ws_workspace/wards.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 r = requests.get("https://data.cityofchicago.org/resource/ydr8-5enu.json?$where=permit_type='PERMIT - NEW CONSTRUCTION'")
 x = r.json()
+
+df = pd.DataFrame(x[:])
+
 df['application_start_date'] = pd.to_datetime(df['application_start_date'])
 df['month_year'] = df['application_start_date'].dt.to_period('M')
 df2 = df.groupby(['month_year','ward']).size().sort_values(ascending=False).reset_index(name="count")
 df2["count"] =df2["count"]+1
 #need to enter path
-df.to_json(r'path\month_ward_permit.json')
+df2.to_json(r'path\month_ward_permit.json')
